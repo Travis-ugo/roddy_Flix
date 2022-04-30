ad3439c391f407a1969d8fd4e277a3388b6ea3fa
Travis-ugo <okonicha.ugo@gmail.com>
2022-05-01 00:18:28 +0100

    setting up auth views

---
+++ lib/views/auth_views/login_roddy.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LogInToRoddy extends StatelessWidget {
+  const LogInToRoddy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
